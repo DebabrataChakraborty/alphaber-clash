beacc64d9c64f63b79315519791df4b307698afe
debabrataChakraborty <debabrata15-674@diu.edu.bd>
2024-09-11 11:29:36 +0600

    complete

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -23,16 +23,56 @@ function continueGame() {
 function handleKeyboardButtonPress(event) {
     const playerPressed = event.key;
     console.log('player pressed',playerPressed);
+
+
+if(playerPressed === 'Escape'){
+    gameOver();
+}
+
+
     const currentAlphabetElement = document.getElementById('current-alphabet')
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
 
     if (playerPressed === expectedAlphabet) {
         console.log('you win')
+        continueGame();
+        removeBackgroundColorById(expectedAlphabet);
+
+        const currentScore =getTextElementValueById('current-score');
+        const newScore =currentScore + 1;
+        setTextElementValueById('current-score',newScore)
+
+
     }
     else {
-        console.log('try again!')
+       const currentLife=getTextElementValueById('current-life');
+       const newLife = currentLife - 1;
+       setTextElementValueById('current-life',newLife)
+
+       if(newLife === 0){
+       gameOver();
+       }
+       
     }
 }
 
-document.addEventListener('keyup', handleKeyboardButtonPress)
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    const totalScore = getTextElementValueById('current-score');
+    setTextElementValueById('total-score',totalScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+
+  
+}
+function playAgain(){
+    hideElementById('final-score');
+    showElementById('play-ground');
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
+}
